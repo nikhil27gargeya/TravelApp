c1de69fe50bea84842e4d510756bf4d532fa8a1b
Nikhil Gargeya <gargeya@usc.edu>
2024-10-28 21:11:55 -0700

    Initial Commit

---
+++ testapp/testappApp.swift
@@ -0,0 +1,17 @@
+//
+//  testappApp.swift
+//  testapp
+//
+//  Created by Nikhil Gargeya on 10/28/24.
+//
+
+import SwiftUI
+
+@main
+struct testappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
